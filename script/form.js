1d732bb9e60d1ce097b19b4b3dddac6deae45664
Shimizu <koosuke.shimizu@gmail.com>
2022-03-16 16:47:21 +0900

    フォームアップデート

--- script/form.js
+++ script/form.js
@@ -25,20 +25,20 @@ $(function () {
 
 
 
-//passwordView
-$(".password-view").click(function () {
-  // iconの切り替え
-  $(this).toggleClass("is-view");
+// //passwordView
+// $(".password-view").click(function () {
+//   // iconの切り替え
+//   $(this).toggleClass("is-view");
 
-  // 入力フォームの取得
-  var input = $(this).prev("input");
-  // type切替
-  if (input.attr("type") == "password") {
-    input.attr("type", "text");
-  } else {
-    input.attr("type", "password");
-  }
-});
+//   // 入力フォームの取得
+//   var input = $(this).prev("input");
+//   // type切替
+//   if (input.attr("type") == "password") {
+//     input.attr("type", "text");
+//   } else {
+//     input.attr("type", "password");
+//   }
+// });
 
 
 //buttonActive
